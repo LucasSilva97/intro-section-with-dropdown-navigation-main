c6988836d5bf167a319c0f4eeaa6301344438627
LucasSilva97 <lucas-silva-santana@hotmail.com>
2023-02-05 00:13:18 -0300

    add event buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log('testando JavaScript...')
+const btnCollapseNavMenu = document.querySelector("#btn-collapse-nav-menu");
+const menuNavigation = document.querySelector(".menu-navigation");
+const btnclosenavMenu = document.querySelector("#btn-close-nav-menu");
+
+btnCollapseNavMenu.addEventListener('click', ()=>{
+    menuNavigation.classList.remove('dp-none');
+
+    btnclosenavMenu.addEventListener('click',()=>{
+        menuNavigation.classList.add('dp-none');
+    })
+}) 
+
