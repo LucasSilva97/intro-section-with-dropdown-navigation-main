e048e898f306981680c6cfd5fb0c348b220be87c
LucasSilva97 <lucas-silva-santana@hotmail.com>
2023-02-06 22:31:39 -0300

    add: nav arrows with event show and hide anchors

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const btnCollapseNavMenu = document.querySelector("#btn-collapse-nav-menu");
 const menuNavigation = document.querySelector(".menu-navigation");
 const btnclosenavMenu = document.querySelector("#btn-close-nav-menu");
+const navFeature = document.querySelector("#nav-feature");
+const navCompany = document.querySelector("#nav-company")
 
 btnCollapseNavMenu.addEventListener('click', ()=>{
     menuNavigation.classList.remove('dp-none');
@@ -10,3 +12,36 @@ btnCollapseNavMenu.addEventListener('click', ()=>{
     })
 }) 
 
+
+function changeArrow(arrow){
+    
+    if(arrow.classList.contains("arrow-down")){
+        arrow.src = "./images/icon-arrow-up.svg";
+        arrow.classList.remove("arrow-down");
+    } else {
+        arrow.src = "./images/icon-arrow-down.svg";
+        arrow.classList.add("arrow-down");
+    }
+}
+
+function showAndHideDivAnchors(divAnchor){
+
+    if(divAnchor.classList.contains("dp-none")){
+        divAnchor.classList.remove("dp-none");
+        divAnchor.classList.add("dp-flex");
+    } else {
+        divAnchor.classList.remove("dp-flex");
+        divAnchor.classList.add("dp-none")
+    }
+
+    console.log(divAnchor.classList.contains("dp-none"));
+}
+
+navFeature.onclick = ()=>{
+    changeArrow(document.getElementById("arrow-feactures"));
+    showAndHideDivAnchors(document.getElementById("features-anchors"));
+}
+navCompany.onclick = ()=>{
+    changeArrow(document.getElementById("arrow-company"));
+    showAndHideDivAnchors(document.getElementById("company-anchors"));
+}
